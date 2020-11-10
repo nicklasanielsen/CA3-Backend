0b55b29bf2046d1025c58d2eac9acfeaa4c55a41
Nikolaj <cph-nl174@cphbusiness.dk>
2020-11-10 09:47:35 +0100

    Entity Role

--- application/src/main/java/entities/Role.java
+++ application/src/main/java/entities/Role.java
@@ -1,43 +1,43 @@
 package entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 /**
  *
- * @author Plaul
+ * @author Nikolaj Larsen
  */
 @Entity
-@Table(name = "roles")
+@Table(name = "ROLES")
 public class Role implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @Basic(optional = false)
     @NotNull
-    @Column(name = "role_name", length = 20)
+    @Column(name = "ROLE_NAME", length = 20)
     private String roleName;
     
-    @ManyToMany(mappedBy = "roleList")
+    @ManyToMany(mappedBy = "roles")
     private List<User> userList;
-
-    public Role() {
-    }
 
     public Role(String roleName) {
         this.roleName = roleName;
+        userList = new ArrayList<>();
     }
 
+    public Role() {
+    }
+    
     public String getRoleName() {
         return roleName;
     }
@@ -52,5 +52,46 @@ public class Role implements Serializable {
 
     public void setUserList(List<User> userList) {
         this.userList = userList;
-    }   
+    }
+    
+    public void addUser(User user) {
+        if(!userList.contains(user)){
+            userList.add(user);
+            user.addRole(this);
+        }        
+    }
+    
+    public void removeUser(User user) {
+        if(userList.contains(user)){
+            userList.remove(user);
+            user.removeRole(this);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + Objects.hashCode(this.roleName);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Role other = (Role) obj;
+        if (!Objects.equals(this.roleName, other.roleName)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
