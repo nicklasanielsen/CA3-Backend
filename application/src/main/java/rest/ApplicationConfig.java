a93ea75fcbb0f5b629270a1444caa65babfc492d
Nicklas Nielsen <nicklasnielsen2770@gmail.com>
2020-11-11 13:07:03 +0100

    Added UserNotFoundExceptionMapper

--- application/src/main/java/rest/ApplicationConfig.java
+++ application/src/main/java/rest/ApplicationConfig.java
@@ -25,6 +25,7 @@ public class ApplicationConfig extends Application {
         resources.add(errorhandling.mappers.GenericExceptionMapper.class);
         resources.add(errorhandling.mappers.NotAuthorizedExceptionMapper.class);
         resources.add(errorhandling.mappers.UserCreationExceptionMapper.class);
+        resources.add(errorhandling.mappers.UserNotFoundExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.AuthenticationResource.class);
         resources.add(rest.UserResource.class);
